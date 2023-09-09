05a565a27b213874e864e4fd6c28fed41bdba15a
Eduardo Cabezudo Vilhalba <vilhalba.20000@gmail.com>
2023-09-09 14:03:57 -0300

    Primeiro endpoint

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,15 +1,35 @@
+import "reflect-metadata";
 import cors from "cors";
-import express, { Request, Response } from "express";
+import "express-async-errors";
+import express, { Request, Response, NextFunction } from "express";
 import "dotenv/config";
+import routes from "./routes";
+import AppError from "@shared/errors/AppError";
+
+import "@shared/infra/prisma";
+import "@shared/container";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.post("/api", (request: Request, response: Response) => {
-  const nome = request.body.nome;
-  return response.json({ ola: `ola mundo ${nome}` }).send();
+app.use("/api", routes);
+
+app.use((err: Error, req: Request, res: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      status: "error",
+      message: err.message,
+    });
+  }
+
+  console.log(err);
+
+  return res.status(500).json({
+    status: "error",
+    message: "internal server error",
+  });
 });
 
 const porta = process.env.PORT || 3333;
